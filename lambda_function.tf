974352152c31a1874d28a0492f33369f8e262a09
abylch <abylch@hotmail.com>
2023-12-28 19:43:49 +0200

    IAM permissions resource aws_iam_policy lambda_function_policy

--- lambda_function.tf
+++ lambda_function.tf
@@ -50,153 +50,42 @@ resource "aws_iam_policy" "lambda_function_policy" {
       "Resource": "${aws_lambda_function.lambda_function.arn}"
     },
     {
-        "Effect": "Allow",
-        "Action": [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource": "arn:aws:logs:*:*:*"
-      },
-    {
-      "Action": ["ssm:DescribeParameters"],
-      "Resource": "*",
-      "Effect": "Allow"
-    },
-    {
-      "Effect" : "Allow",
-      "Action" : ["ssm:GetParameter"],
-      "Resource" : ["*"]
-    },
-    {
-      "Action": ["ssm:GetParameters"],
-      "Resource": "arn:aws:ssm:us-west-1:redacted:parameter/*",
-      "Effect": "Allow"
-    },
-    {
-      "Effect" : "Allow",
-      "Action" : ["dynamodb:*"],
-      "Resource" : "${aws_dynamodb_table.user_table.arn}"
-    },
-    {
-      "Sid": "ListObjectsInBucket",
       "Effect": "Allow",
-      "Action": ["s3:ListBucket"],
-      "Resource": ["*"]
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+      ],
+      "Resource": "arn:aws:logs:*:*:*"
     },
     {
-      "Sid": "AllObjectActions",
       "Effect": "Allow",
-      "Action": "s3:*Object",
-      "Resource": ["*"]
+      "Action": ["ssm:GetParameter"],
+      "Resource": "arn:aws:ssm:${var.region}:${var.account_id}:parameter/${var.parameter_name}"
     },
     {
-            "Action": [
-                "dynamodb:*",
-                "dax:*",
-                "application-autoscaling:DeleteScalingPolicy",
-                "application-autoscaling:DeregisterScalableTarget",
-                "application-autoscaling:DescribeScalableTargets",
-                "application-autoscaling:DescribeScalingActivities",
-                "application-autoscaling:DescribeScalingPolicies",
-                "application-autoscaling:PutScalingPolicy",
-                "application-autoscaling:RegisterScalableTarget",
-                "cloudwatch:DeleteAlarms",
-                "cloudwatch:DescribeAlarmHistory",
-                "cloudwatch:DescribeAlarms",
-                "cloudwatch:DescribeAlarmsForMetric",
-                "cloudwatch:GetMetricStatistics",
-                "cloudwatch:ListMetrics",
-                "cloudwatch:PutMetricAlarm",
-                "cloudwatch:GetMetricData",
-                "datapipeline:ActivatePipeline",
-                "datapipeline:CreatePipeline",
-                "datapipeline:DeletePipeline",
-                "datapipeline:DescribeObjects",
-                "datapipeline:DescribePipelines",
-                "datapipeline:GetPipelineDefinition",
-                "datapipeline:ListPipelines",
-                "datapipeline:PutPipelineDefinition",
-                "datapipeline:QueryObjects",
-                "ec2:DescribeVpcs",
-                "ec2:DescribeSubnets",
-                "ec2:DescribeSecurityGroups",
-                "iam:GetRole",
-                "iam:ListRoles",
-                "kms:DescribeKey",
-                "kms:ListAliases",
-                "sns:CreateTopic",
-                "sns:DeleteTopic",
-                "sns:ListSubscriptions",
-                "sns:ListSubscriptionsByTopic",
-                "sns:ListTopics",
-                "sns:Subscribe",
-                "sns:Unsubscribe",
-                "sns:SetTopicAttributes",
-                "lambda:CreateFunction",
-                "lambda:ListFunctions",
-                "lambda:ListEventSourceMappings",
-                "lambda:CreateEventSourceMapping",
-                "lambda:DeleteEventSourceMapping",
-                "lambda:GetFunctionConfiguration",
-                "lambda:DeleteFunction",
-                "resource-groups:ListGroups",
-                "resource-groups:ListGroupResources",
-                "resource-groups:GetGroup",
-                "resource-groups:GetGroupQuery",
-                "resource-groups:DeleteGroup",
-                "resource-groups:CreateGroup",
-                "tag:GetResources",
-                "kinesis:ListStreams",
-                "kinesis:DescribeStream",
-                "kinesis:DescribeStreamSummary"
-            ],
-            "Effect": "Allow",
-            "Resource": "*"
-        },
-        {
-            "Action": "cloudwatch:GetInsightRuleReport",
-            "Effect": "Allow",
-            "Resource": "arn:aws:cloudwatch:*:*:insight-rule/DynamoDBContributorInsights*"
-        },
-        {
-            "Action": [
-                "iam:PassRole"
-            ],
-            "Effect": "Allow",
-            "Resource": "*",
-            "Condition": {
-                "StringLike": {
-                    "iam:PassedToService": [
-                        "application-autoscaling.amazonaws.com",
-                        "application-autoscaling.amazonaws.com.cn",
-                        "dax.amazonaws.com"
-                    ]
-                }
-            }
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "iam:CreateServiceLinkedRole"
-            ],
-            "Resource": "*",
-            "Condition": {
-                "StringEquals": {
-                    "iam:AWSServiceName": [
-                        "replication.dynamodb.amazonaws.com",
-                        "dax.amazonaws.com",
-                        "dynamodb.application-autoscaling.amazonaws.com",
-                        "contributorinsights.dynamodb.amazonaws.com",
-                        "kinesisreplication.dynamodb.amazonaws.com"
-                    ]
-                }
-            }
-        }
+      "Effect": "Allow",
+      "Action": [
+        "dynamodb:PutItem",
+        "dynamodb:GetItem",
+        "dynamodb:UpdateItem",
+        "dynamodb:Query"
+      ],
+      "Resource": "${aws_dynamodb_table.user_table.arn}"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject"
+      ],
+      "Resource": "${aws_s3_bucket.user_bucket.arn}/*"
+    }
   ]
 }
 EOF
 }
+
 
 resource "aws_iam_role_policy_attachment" "lambda_function_policy_attachment" {
   role       = aws_iam_role.lambda_function_role.name
